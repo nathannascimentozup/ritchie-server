6d77156f921f41ff9ec08a2ee3eb8849486810ac
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-04-24 09:23:02 -0300

    adding first pipeline for testing

--- run-tests-ci.sh
+++ run-tests-ci.sh
@@ -17,7 +17,6 @@ export KEYCLOAK_URL=http://keycloak:8080
 export OAUTH_URL=http://keycloak:8080/auth/realms/ritchie
 export CLI_VERSION_URL=http://stubby4j:8882/s3-version-mock
 
-mkdir -p $TEST_RESULTS_DIR
 gotestsum --format=short-verbose --junitfile $TEST_RESULTS_DIR/gotestsum-report.xml -- -p 2 -coverprofile=coverage.txt `go list ./... | grep -v vendor/`
 
 ls -lah bin
